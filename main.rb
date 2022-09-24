f902cdb074e776ed999eaf05099ae641512fa1d0
Jonathan East <jeast3113@gmail.com>
2022-09-24 19:43:54 -0400

    created method

---
+++ main.rb
@@ -0,0 +1,8 @@
+# Create a caesar cipher method that returns a string and shifts the string
+# by a set number and returns the new string
+# method takes two parameters(a string, and a number)
+
+
+def caesar_cipher(string, shift)
+  
+end
