63f5a75b8685d647adf8b727c44adb61498b411e
Jonathan East <jeast3113@gmail.com>
2022-09-25 12:32:52 -0400

    created variables to set base numbers for 'a' and 'A' for algorithms

--- main.rb
+++ main.rb
@@ -4,17 +4,24 @@
 
 
 def caesar_cipher(string, shift)
+  base_upper_A = "A".ord
+  base_lower_a = "a".ord
+  alpha_base = 26 # number of letters in alphabet
+
+  p base_upper_A
+  p base_lower_a
+
   shifted_word = []
   p ord_array = string.chars.map(&:ord)
   ord_array.each do |num|
-    shifted_number = num + shift
-    if num == 32
+    #shifted_number = num + shift # update this and separate it from 'A' and 'a'
+    if num == 32 # keep this for spacing in strings
       shifted_word << " "
     else
-      shifted_word << num.chr
+      shifted_word << num.chr # keep this for other characters not being met from conditions
     end
   end
-  p shifted_word.join
+  shifted_word.join
 end
 
 p caesar_cipher("What a string!", 5)
