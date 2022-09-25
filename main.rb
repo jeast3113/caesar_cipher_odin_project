2f82e3104c30dd7c84bbdfdd9e7baf91f24ffdf4
Jonathan East <jeast3113@gmail.com>
2022-09-25 12:54:01 -0400

    commented out the testing and checked the letter for the test word provided from Odin Project; currently working and will implement the algorithm for the lower case letters

--- main.rb
+++ main.rb
@@ -11,12 +11,12 @@ def caesar_cipher(string, shift)
   # base_upper_A
   # base_lower_a
 
-  num = "Z".ord # example for math algorithm - number should be '66'
-  shift = 5
+  # num = "Z".ord # example for math algorithm - number should be '66'
+  # shift = 5
   # num
 
   # test math algorithm for getting number for chr
-  p ((num - base_upper_A + shift) % alpha_base + base_upper_A).chr
+  # p ((num - base_upper_A + shift) % alpha_base + base_upper_A).chr
 
   shifted_word = []
   ord_array = string.chars.map(&:ord)
@@ -26,7 +26,7 @@ def caesar_cipher(string, shift)
       shifted_word << " "
     elsif num >= 65 && num <= 90
       shifter_upper_A = (num - base_upper_A + shift) % alpha_base + base_upper_A
-      shifted_word << shifter_upper_A
+      shifted_word << shifter_upper_A.chr
     else
       shifted_word << num.chr # keep this for other characters not being met from conditions
     end
