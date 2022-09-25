84fe153e71948a6017cc54b9b1e3bd4eaeedf9a0
Jonathan East <jeast3113@gmail.com>
2022-09-24 23:36:12 -0400

    saving work and will figure out the modulo process for the algorithm

--- main.rb
+++ main.rb
@@ -5,25 +5,25 @@
 
 def caesar_cipher(string, shift)
   shifted_word = []
-  ord_array = string.chars.map(&:ord)
+  p ord_array = string.chars.map(&:ord)
   ord_array.each do |num|
     shifted_number = num + shift
     if num == 32
       shifted_word << " "
-    elsif shifted_number > 90 && shifted_number < 97
+    elsif (shifted_number >= 90 && shifted_number <= 97)
       shifted_word << (shifted_number - 26).chr
-    elsif shifted_number > 122
-      shifted_word << (shifted_number - 26).chr
-    elsif num >= 97 && num <= 122
+    # elsif (shifted_number > 122)
+    #   shifted_word << (shifted_number - 26).chr
+    elsif (shifted_number >= 65 && shifted_number <= 90) || (shifted_number >= 97 && shifted_number <= 122)
       shifted_word << (shifted_number).chr
     else
       shifted_word << num.chr
     end
   end
-  shifted_word.join
+  p shifted_word.join
 end
 
-p caesar_cipher("zzzzzZZZZ!?@?", 1)
+p caesar_cipher("What a string!", 5)
 # p 122 - 97 # = 25
 # p "A".ord
 # p "Z".ord
